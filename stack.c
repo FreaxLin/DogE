fad94442756a28f0080824409b0bbcda2d515d3e
sai <576920627@qq.com>
2020-06-23 10:01:28 +0800

    修改栈帧

--- stack.c
+++ stack.c
@@ -7,6 +7,32 @@ typedef struct {
     void* local_var;
 }stack_slot;
 
+typedef struct{
+    int size;
+    int count;
+    void* header;
+    void* stack;
+}operate_stack;
+
+typedef struct {
+    stack_slot local_var_array;
+    operate_stack os;
+}stack_frame;
+
+stack_slot init_local_var(int num, int size, int* index){
+    stack_slot slot;
+    slot.offset = (int*) malloc(num * sizeof(int));
+    for (int i = 0; i < num; i++){
+        slot.offset[i] = index[i];
+    }
+    slot.local_var = malloc(sizeof(size));
+    return slot;
+}
+void destory_local_var(stack_slot* slot){
+    free(slot->offset);
+    free(slot->local_var);
+}
+
 void* get_local_var_value(stack_slot* slot, int index, void* value){
     int i = slot->offset[index];
     void* position = slot->offset + i;
@@ -21,38 +47,66 @@ void set_local_var_value(stack_slot* slot, int index, void* value){
     }
 }
 
-stack_slot init_local_var(int num, int size){
-    stack_slot slot;
-    slot.offset = (int*) malloc(num * sizeof(int));
-    slot.local_var = malloc(sizeof(size));
-    return slot;
+operate_stack init_operate_stack(int size){
+    operate_stack os;
+    os.size = size;
+    os.count = 0;
+    os.stack = malloc(size);
+    os.header = os.stack;
+    return os;
 }
 
-void destory_local_var(stack_slot* slot){
-    free(slot->offset);
-    free(slot->local_var);
+void destory_operate_stack(operate_stack* os){
+    free(os->header);
 }
 
-typedef struct {
-    stack_slot local_var_array;
-    void* operate_stack;
-}stack_frame;
+void push_operate_stack_int(operate_stack* os, int value){
+    *((int*)os->stack) = value;
+    os->stack = os->stack + sizeof(int);
+    os->count = os->count + 1;
+}
 
-void push(int index){
-    
+void push_operate_stack_char(operate_stack* os, char c){
+    *((char*)os->stack) = c;
+    os->stack = os->stack + sizeof(char);
+    os->count = os->count + 1;
+}
+
+int add_int(operate_stack* os){
+    os->stack = os->stack - sizeof(int);
+    int i = *(int*)(os->stack);
+    os->count = os->count - 1;
+    os->stack = os->stack - sizeof(int);
+    int j = *(int*)(os->stack);
+    os->count = os->count - 1;
+    return i + j;
+}
+
+char add_char(operate_stack* os){
+    os->stack = os->stack - sizeof(char);
+    char i = *(char*)(os->stack);
+    os->count = os->count - 1;
+    os->stack = os->stack - sizeof(char);
+    char j = *(char*)(os->stack);
+    os->count = os->count - 1;
+    return i + j;
+}
+
+stack_frame init_stack_frame(int operate_stack_size, int local_var_num, int local_var_size, int* index){
+    stack_frame sf;
+    sf.local_var_array = init_local_var(local_var_num, local_var_size, index);
+    sf.os = init_operate_stack(operate_stack_size);
+    return sf;
+}
+
+
+void run_command(char* command){
+
 }
 
 
 
 int main(int argc, char **argv){
-    int x = 0;
-    int y = 1;
-    int c = x + y;
-    // set_0 0;
-    // set_1 1;
-    (1 + 2) * 3 / 2;
     
 
-
-    
-}
+}   
